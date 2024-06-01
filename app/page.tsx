bf92b807f0e34e73830942489bb1dacfc0348d48
Sandeepa Sineth <sineth21404@gmail.com>
2024-06-01 23:17:28 +0530

    All design completed

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,23 @@ import RecentProjects from "./Components/RecentProjects";
 import React from "react";
 import { navItems } from "@/data";
 import Clients from "./Components/ui/Clients";
+import Experience from "./Components/Experience";
+import Approach from "./Components/Approach";
+import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center 
-    items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero/>
         <Grid/>
         <RecentProjects/>
         <Clients/>
+        <Experience/>
+        <Approach/>
+        <Footer/>
       </div>
     </main>
   );
